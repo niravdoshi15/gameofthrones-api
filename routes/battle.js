35b9b6cce4d715c3cde77374b916adb04636ab8e
niravdoshi15 <niravdoshi15@gmail.com>
2020-03-24 20:14:46 +0530

    all routes

--- routes/battle.js
+++ routes/battle.js
@@ -9,7 +9,7 @@ router.get('/list', function (req, res) {
         if (err) res.json(err)
         else {
             var list = []
-            docs.map(b => list.push(b.name))          
+            docs.map(b => list.push(b.name))
             res.json(list)
         }
     })
@@ -22,6 +22,41 @@ router.get('/count', function (req, res) {
             res.json(docs.length)
         }
     })
+})
+
+// router.get('/search', async function (req, res) {
+//     var result = []
+//     searchQuery = ['attacker_king', 'defender_king']
+//     for (let i = 0; i < searchQuery.length; i++) {
+//         await Battle.find({ [searchQuery[i]]: req.query.king }, function (err, docs) {
+//             if (err) res.json(err)
+//             else {
+//                 result.push(...docs)
+//             }
+//         })
+//     }
+//     res.json(result)
+// })
+
+router.get('/search', async function (req, res) {
+    var result = []
+    if (req.query.location && req.query.type) {
+        await Battle.find({ $and: [{ $or: [{ 'attacker_king': req.query.king }, { 'defender_king': req.query.king }] }, { 'location': req.query.location || '' }, { 'battle_type': req.query.type || '' }] }, function (err, docs) {
+            if (err) res.json(err)
+            else {
+                result.push(...docs)
+            }
+        })
+    }
+    else if (req.query.king) {
+        await Battle.find({ $or: [{ 'attacker_king': req.query.king }, { 'defender_king': req.query.king }] }, function (err, docs) {
+            if (err) res.json(err)
+            else {
+                result.push(...docs)
+            }
+        })
+    }
+    res.json(result)
 })
 
 module.exports = router;
